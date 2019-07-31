ad8ba51c36c92d12ab5e5ac73ddc9b3f6f34ab20
Aditi Singh <rishipandya44@gmail.com>
2019-07-31 10:39:34 -0700

    Login validation and landing started

--- js/login.js
+++ js/login.js
@@ -8,3 +8,32 @@ loginButton.onclick = function() {
 registerButton.onclick = function() {
 	document.querySelector("#flipper").classList.toggle("flip");
 };
+
+$('#login').click(function () {
+	// alert('hi');
+
+	var username = $('#username').val().trim();
+	var password = $('#password').val().trim();
+
+	if (username && password) {
+		if (!(username.length > 6)) {
+			alert('Enter username greater than 6 characters');
+		}
+		if (!(password.length > 8)) {
+			alert('Enter password greater than 8 characters');
+		}
+		if (username.length > 6 && password.length > 8) {
+			alert('Success!')
+		}
+	} else {
+		alert('Please enter all the fields')
+	}
+
+});
+
+
+
+
+
+
+//
