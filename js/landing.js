4617abcbdba792b22ae6be06b534175f0b125874
Aditi Singh <rishipandya44@gmail.com>
2019-08-21 10:44:52 -0700

    maps done

---
+++ js/landing.js
@@ -0,0 +1,14 @@
+
+var map = new ol.Map({
+  target: 'map',
+  size: [10, 10],
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([72.870793, 19.021478]),
+    zoom: 17
+  })
+});
