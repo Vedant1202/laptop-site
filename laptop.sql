dabde8aa8644d082b3d6c9080a728a92fac2bc7e
Vedant1202 <Vedant1202@users.noreply.github.com>
2019-09-30 00:12:59 +0530

    SQL file update

--- laptop.sql
+++ laptop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2019 at 01:55 PM
+-- Generation Time: Sep 29, 2019 at 08:42 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
