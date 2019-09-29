95466a370919a8e1c1ed891d45187013c52f6746
Vedant1202 <Vedant1202@users.noreply.github.com>
2019-09-29 17:21:41 +0530

    fetchall

--- backend/api/product/fetchall.php
+++ backend/api/product/fetchall.php
@@ -11,9 +11,8 @@ include "../connection.php";
 if ($conn->connect_error) {
   echo "Connection failed: " . $conn->connect_error;
 } else {
-  if ( isset($_POST["pid"]) && !empty($_POST["pid"])){    // code...
 
-    $sql = "SELECT * FROM product ORDER BY pid DESC";  
+    $sql = "SELECT * FROM product ORDER BY pid DESC";
     $result = $conn->query($sql);
 
     if (!mysqli_query($conn, $sql)) {
@@ -45,11 +44,6 @@ if ($conn->connect_error) {
 
       // echo json_encode([json_decode(statusMessage(200, "success")), $res]);
     }
-  } else {
-    // code...
-    echo statusMessage(400, "bad request");
-
-  }
 
 
 
