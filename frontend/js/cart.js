2635baca815d8e7fff2d9f6d80364b42ec62f17b
Vedant1202 <Vedant1202@users.noreply.github.com>
2019-10-03 15:53:09 +0530

    commit

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,19 +1,3 @@
 $(document).ready(function () {
-
-  $.ajax({
-    type: "POST",
-    url: apiurl + '/cart/fetch.php',
-    data: {
-      'uid': getData('user')[1].uid
-    },
-    success: function(data) {
-      console.log('done');
-      
-    },
-   error: function(error) {
-     console.log(error);
-   },
-   dataType: 'json'
-  });
-
+  fetchCart(getData('user')[1].uid);
 });
