064655a185f8e0e92d24117eea74d99b83bed1a0
Vedant1202 <Vedant1202@users.noreply.github.com>
2019-10-04 15:21:17 +0530

    Empty cart done

---
+++ backend/api/cart/empty.php
@@ -0,0 +1,42 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include "../functions.php";
+include "../connection.php";
+
+
+// Check connection
+if ($conn->connect_error) {
+  echo "Connection failed: " . $conn->connect_error;
+} else {
+
+  if ( isset($_POST["uid"]) && !empty($_POST["uid"])) {
+
+    $uid = $_POST["uid"];
+
+    $sql = "DELETE FROM cart WHERE uid=$uid;";
+
+    if (!mysqli_query($conn, $sql)) {
+      echo statusMessage(500, "internal server error");
+      die('Error: ' . mysqli_error($conn));
+
+    } else {
+      // echo "New record created successfully";
+      $conn->close();
+      $res = [
+        'removed' => TRUE
+      ];
+      echo json_encode([json_decode(statusMessage(200, "success")), $res]);
+    }
+
+  } else {
+    echo statusMessage(400, "bad request");
+  }
+
+}
+
+
+
+ ?>
--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -5,3 +5,27 @@ $(document).ready(function () {
 
   fetchCart(getData('user')[1].uid);
 });
+
+
+function emptyCart() {
+  $.ajax({
+    type: "POST",
+    url: apiurl + '/cart/empty.php',
+    data: {
+      'uid': getData('user')[1].uid,
+    },
+    success: function(data) {
+      alert('Order placed succesgfully');
+      Nav.assign('products.html');
+    },
+   error: function(error) {
+     console.log(error);
+   },
+   dataType: 'json'
+  });
+}
+
+
+
+
+//
